3606768f16227a0060685b9fa1f439cf4634ef6d
Jeremy Rice <jdavidrice@gmail.com>
2021-02-07 20:57:53 -0500

    Create new branch develop to hold recent changes

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -49,30 +49,30 @@ document.addEventListener("DOMContentLoaded", (event) => {
     createBurgerBtn.addEventListener("submit", (e) => {
       e.preventDefault();
 
-      // Grabs the value of the textarea that goes by the name, "quote"
-      const newBurger = {
-        name: document.getElementById("bu").value.trim(),
-        devoured: document.getElementById("devoured").checked,
-      };
+      // Grabs the value of the textarea that goes by the name, "bu"
+        const newBurger = {
+          name: document.getElementById("bu").value.trim(),
+          devoured: document.getElementById("devoured").checked,
+        };
 
-      // Send POST request to create a new quote
-      fetch("/api/burgers", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
+        // Send POST request to create a new quote
+        fetch("/api/burgers", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
 
-        // make sure to serialize the JSON body
-        body: JSON.stringify(newBurger),
-      }).then(() => {
-        // Empty the form
-        document.getElementById("bu").value = "";
+          // make sure to serialize the JSON body
+          body: JSON.stringify(newBurger),
+        }).then(() => {
+          // Empty the form
+          document.getElementById("bu").value = "";
 
-        // Reload the page so the user can see the new burger
-        console.log("Created a new burger!");
-        location.reload();
-      });
+          // Reload the page so the user can see the new burger
+          console.log("Created a new burger!");
+          location.reload();
+        });
     });
   }
 
