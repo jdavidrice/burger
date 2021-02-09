6de8451da79fb10ea143e81e0791a23098c82432
Jeremy Rice <jdavidrice@gmail.com>
2021-02-09 14:30:05 -0500

    Change connection.js to use JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burger_db",
-});
-
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "burger_db",
+// });
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burger_db"
+  })
+};
 // Make connection.
 connection.connect((err) => {
   if (err) {
